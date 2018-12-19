a9300ce75b59b330ad468705bdb6ee451bf7f07b
Sebastian Y. Tabares <sytabaresa@gmail.com>
2018-12-19 18:26:03 -0500

    Addes arduion Platdformio project sepparated in files

---
+++ Arduino_IDE/dlmsArduinoDemo/include/connection.h
@@ -0,0 +1,65 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+#include <dlmssettings.h>
+#include <variant.h>
+#include <cosem.h>
+#include <client.h>
+#include <converters.h>
+#include <gxobjects.h>
+
+//Received data.
+extern gxByteBuffer frameData;
+extern dlmsSettings meterSettings;
+
+//Initialize connection to the meter.
+int com_initializeConnection();
+
+int com_close();
+
+int com_readSerialPort(unsigned char eop);
+
+int com_readDataBlock(message *messages, gxReplyData *reply);
+
+//Report error on output;
+void com_reportError(const char *description, gxObject *object, unsigned char attributeOrdinal, int ret);
+
+//Get Association view.
+int com_getAssociationView();
+
+//Read object.
+int com_read(gxObject *object, unsigned char attributeOrdinal);
+
+int com_write(gxObject *object, unsigned char attributeOrdinal);
+
+int com_method(gxObject *object, unsigned char attributeOrdinal, dlmsVARIANT *params);
+
+//Read objects.
+int com_readList(gxArray *list);
+
+int com_readRowsByEntry(gxProfileGeneric *object, unsigned long index, unsigned long count);
+
+int com_readRowsByRange(gxProfileGeneric *object, struct tm *start, struct tm *end);
+
+//Read scalers and units. They are static so they are read only once.
+int com_readScalerAndUnits();
+
+//Read profile generic columns. They are static so they are read only once.
+int com_readProfileGenericColumns();
+
+//Read profile generics rows.
+int com_readProfileGenerics();
+
+int com_readValue(gxObject *object, unsigned char index);
+
+// This function reads ALL objects that meter have excluded profile generic objects.
+// It will loop all object's attributes.
+int com_readValues();
+
+//This function reads ALL objects that meter have. It will loop all object's attributes.
+int com_readAllObjects();
+
+// Read DLMS Data frame from the device.
+int readDLMSPacket(gxByteBuffer *data, gxReplyData *reply);
+
+#endif
---
+++ Arduino_IDE/dlmsArduinoDemo/src/main.cpp
@@ -0,0 +1,115 @@
+
+
+//
+// --------------------------------------------------------------------------
+//  Gurux Ltd
+//
+//
+//
+//
+// Version:         $Revision: 10346 $,
+//                  $Date: 2018-10-29 16:08:18 +0200 (Mon, 29 Oct 2018) $
+//                  $Author: gurux01 $
+//
+// Copyright (c) Gurux Ltd
+//
+//---------------------------------------------------------------------------
+//
+//  DESCRIPTION
+//
+// This file is a part of Gurux Device Framework.
+//
+// Gurux Device Framework is Open Source software; you can redistribute it
+// and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; version 2 of the License.
+// Gurux Device Framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+//
+// More information of Gurux DLMS/COSEM Director: http://www.gurux.org/GXDLMSDirector
+//
+// This code is licensed under the GNU General Public License v2.
+// Full text may be retrieved at http://www.gnu.org/licenses/gpl-2.0.txt
+//---------------------------------------------------------------------------
+
+#include <Arduino.h>
+
+#include <dlmssettings.h>
+#include <variant.h>
+#include <cosem.h>
+#include <client.h>
+#include <converters.h>
+#include <gxobjects.h>
+
+#include "connection.h"
+
+
+//Client don't need this.
+unsigned char svr_isTarget(
+    dlmsSettings *settings,
+    unsigned long serverAddress,
+    unsigned long clientAddress)
+{
+  return 0;
+}
+
+//Client don't need this.
+int svr_connected(
+    dlmsServerSettings *settings)
+{
+  return 0;
+}
+
+void time_now(
+    gxtime *value)
+{
+  //Get local time somewhere.
+  time_t tm1 = time(NULL);
+  struct tm dt = *localtime(&tm1);
+  time_init2(value, &dt);
+}
+
+gxClock clock1;
+
+// Custom settings for the connection
+#define LOGICAL_NAMES 1
+#define CLIENT_ADDR 16
+#define SERVER_ADDR 1
+#define AUTH_DLMS DLMS_AUTHENTICATION_NONE
+#define PASS_DLMS NULL
+
+void setup()
+{
+  bb_init(&frameData);
+  //Set frame size.
+  bb_capacity(&frameData, 128);
+  cl_init(&meterSettings, LOGICAL_NAMES, CLIENT_ADDR, SERVER_ADDR, AUTH_DLMS, PASS_DLMS, DLMS_INTERFACE_TYPE_HDLC);
+
+  cosem_init(&clock1.base, DLMS_OBJECT_TYPE_CLOCK, "0.0.1.0.0.255");
+
+  // start serial port at 9600 bps:
+  Serial.begin(9600);
+  while (!Serial)
+  {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+}
+
+void loop()
+{
+  int ret;
+  //Initialize connection.
+  ret = com_initializeConnection();
+  if (ret != DLMS_ERROR_CODE_OK)
+  {
+    return;
+  }
+  //Read clock.
+  ret = com_read(&clock1.base, 2);
+  if (ret != DLMS_ERROR_CODE_OK)
+  {
+    return;
+  }
+  com_close();
+}
